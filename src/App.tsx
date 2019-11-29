9168ab34e352e185beaced2e58ed16492b5cd61b
jacobchwastek <jacobchwastek@gmail.com>
2019-11-29 17:22:42 +0100

    feature branch

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-//I'm in develop branch
+//I'm in feature branch
 const App: React.FC = () => {
   return (
     <div className="App">
