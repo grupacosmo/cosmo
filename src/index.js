48c60496df7a61443e7095be655340faf586ea7f
jacobchwastek <jacobchwastek@gmail.com>
2019-12-12 11:46:08 +0100

    Vulnerability in index.js node-scss is not supported for some Linux dist for ex. Arch

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch } from "react-router-dom";
 
-// import "assets/scss/material-kit-react.scss?v=1.8.0";
+//!! Important!! //
+//Not supported on some linux dist for example  Arch// 
+//import "assets/scss/material-kit-react.scss?v=1.8.0";
 
 // pages for this product
 import Components from "views/Components/Components.js";
