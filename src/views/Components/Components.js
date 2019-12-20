c09d3dea53161522b428e704541d7730e2e521b9
borchowiec <borchowiecpatryk@gmail.com>
2019-12-20 14:48:48 +0100

    Added home section. Added id to 'OurTeam' section.

--- src/views/Components/Components.js
+++ src/views/Components/Components.js
@@ -8,14 +8,13 @@ import { makeStyles } from "@material-ui/core/styles";
 // core components
 import Header from "components/Header/Header.js";
 import Footer from "components/Footer/Footer.js";
-import GridContainer from "components/Grid/GridContainer.js";
-import GridItem from "components/Grid/GridItem.js";
-import Parallax from "components/Parallax/Parallax.js";
+
 // sections for this page
 import HeaderLinks from "components/Header/HeaderLinks.js";
 
 import styles from "assets/jss/material-kit-react/views/components.js";
 import OurTeamSection from "../Components/Sections/OurTeamSection.js";
+import HomeSection from "./Sections/HomeSection";
 
 const useStyles = makeStyles(styles);
 
@@ -35,20 +34,8 @@ export default function Components(props) {
         }}
         {...rest}
       />
-      <Parallax image={require("assets/img/bg4.jpg")}>
-        <div className={classes.container}>
-          <GridContainer>
-            <GridItem>
-              <div className={classes.brand}>
-                <h1 className={classes.title}>Material Kit React.</h1>
-                <h3 className={classes.subtitle}>
-                  A Badass Material-UI Kit based on Material Design.
-                </h3>
-              </div>
-            </GridItem>
-          </GridContainer>
-        </div>
-      </Parallax>
+
+      <HomeSection />
 
       <div className={classNames(classes.main, classes.mainRaised)}>
 
---
+++ src/views/Components/Sections/HomeSection.js
@@ -0,0 +1,45 @@
+import React from "react";
+// react components for routing our app without refresh
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+// @material-ui/icons
+// core components
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import Parallax from "components/Parallax/Parallax.js";
+
+import styles from "assets/jss/material-kit-react/views/components.js";
+import Button from "components/CustomButtons/Button.js";
+import {Link} from "react-scroll";
+
+const useStyles = makeStyles(styles);
+
+export default function HomeSection() {
+    const classes = useStyles();
+    return (
+        <Parallax image={require("assets/img/home_background.jpg")}>
+            <div className={classes.container}>
+                <GridContainer>
+                    <GridItem>
+                        <div className={classes.brand}>
+                            <h1 className={classes.title}>Projekt COSMO</h1>
+                            <h3 className={classes.subtitle}>
+                                Studenckie koło naukowe Politechniki Krakowskiej
+                            </h3>
+                            <Link
+                                activeClass="active"
+                                to="who-we-are"
+                                spy={true}
+                                smooth={true}
+                                offset={-70}
+                                duration= {500}
+                            >
+                                <Button type="button" color="info" round>Kim jesteśmy?</Button>
+                            </Link>
+                        </div>
+                    </GridItem>
+                </GridContainer>
+            </div>
+        </Parallax>
+    );
+}
