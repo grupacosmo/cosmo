040e0e9a0964930b92d056e817866c6900106d0f
jacobchwastek <jacobchwastek@gmail.com>
2019-12-23 19:18:35 +0100

    OurTeamSection Update not ready

--- src/views/Components/Sections/OurTeamSection.js
+++ src/views/Components/Sections/OurTeamSection.js
@@ -6,9 +6,9 @@ import classNames from "classnames";
 import { makeStyles } from "@material-ui/core/styles";
 // @material-ui/icons
 // core components
+import Button from "components/CustomButtons/Button.js";
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
-
 import styles from "assets/jss/material-kit-react/views/landingPageSections/teamStyle.js";
 import Card from "components/Card/Card";
 import profilePhoto from "assets/img/faces/pawel_kisielewicz_profile.jpg";
@@ -16,65 +16,78 @@ import profilePhoto from "assets/img/faces/pawel_kisielewicz_profile.jpg";
 const useStyles = makeStyles(styles);
 
 export default function OurTeamSection(props) {
-    const classes = useStyles();
-    const imageClasses = classNames(
-        classes.imgRaised,
-        classes.imgRoundedCircle,
-        classes.imgFluid
-    );
+  const classes = useStyles();
+  const imageClasses = classNames(
+    classes.imgRaised,
+    classes.imgRoundedCircle,
+    classes.imgFluid
+  );
 
-    //this style and style below is using for centering description paragraph vertically
-    const descriptionGridContainerStyle = {
-        height: "100%"
-    };
+  //this style and style below is using for centering description paragraph vertically
+  const descriptionGridContainerStyle = {
+    height: "100%"
+  };
 
-    const descriptionGridItemStyle = {
-        height: "100%",
-        display: "flex",
-        alignItems: "center"
-    };
+  const descriptionGridItemStyle = {
+    height: "100%",
+    display: "flex",
+    alignItems: "center"
+  };
 
-    const descriptionStyle = {
-        textAlign: "justify",
-        fontFamily: "Roboto Slab",
-        padding: 20
-    };
+  const descriptionStyle = {
+    textAlign: "justify",
+    fontFamily: "Roboto Slab",
+    padding: 20
+  };
 
-    return (
-        <div className={classes.section} id="who-we-are">
-            <div className={classes.container}>
+  return (
+    <div className={classes.section} id="who-we-are">
+      <div className={classes.container}>
+        <h2 className={classes.title}>Kim jesteśmy?</h2>
+        <GridContainer justify="center">
+          <GridItem xs={12} sm={12} md={6}>
+            <GridContainer
+              justify="center"
+              style={descriptionGridContainerStyle}
+            >
+              <GridItem xs={12} sm={12} md={9} style={descriptionGridItemStyle}>
+                <p className={classes.description} style={descriptionStyle}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et
+                  finibus nulla, at tincidunt orci. Pellentesque porttitor
+                  imperdiet arcu, ut posuere mauris mattis dapibus. Curabitur
+                  vitae elit eu elit scelerisque malesuada sed vitae dui.
+                  Curabitur nec arcu non sem aliquet semper ac id orci.
+                  Pellentesque ultrices venenatis pulvinar. Vestibulum commodo
+                  massa in nibh mollis, sed scelerisque elit sagittis. Integer
+                  pulvinar tortor vel convallis faucibus.
+                </p>
+                <Button color="primary" round>
+                  Więcej
+                </Button>
+              </GridItem>
+            </GridContainer>
+          </GridItem>
 
-                <h2 className={classes.title}>Kim jesteśmy?</h2>
-                <GridContainer justify="center">
-                    <GridItem xs={12} sm={12} md={6}>
-                        <GridContainer justify="center" style={descriptionGridContainerStyle}>
-                            <GridItem xs={12} sm={12} md={9} style={descriptionGridItemStyle}>
-                                <p className={classes.description} style={descriptionStyle}>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et finibus nulla, at tincidunt
-                                    orci. Pellentesque porttitor imperdiet arcu, ut posuere mauris mattis dapibus. Curabitur
-                                    vitae elit eu elit scelerisque malesuada sed vitae dui. Curabitur nec arcu non sem aliquet
-                                    semper ac id orci. Pellentesque ultrices venenatis pulvinar. Vestibulum commodo massa in
-                                    nibh mollis, sed scelerisque elit sagittis. Integer pulvinar tortor vel convallis faucibus.
-                                </p>
-                            </GridItem>
-                        </GridContainer>
-                    </GridItem>
-
-                    <GridItem xs={12} sm={12} md={4}>
-                        <Card plain>
-                            <GridItem xs={12} sm={12} md={6} className={classes.itemGrid}>
-                                <img src={profilePhoto} alt="Paweł Kisielewicz" className={imageClasses} />
-                            </GridItem>
-                            <h4 className={classes.cardTitle}>
-                                mgr inż. Paweł Kisielewicz
-                                <br />
-                                <small className={classes.smallTitle}>Prowadzący koła naukowego COSMO</small>
-                            </h4>
-                        </Card>
-                    </GridItem>
-                </GridContainer>
-
-            </div>
-        </div>
-    );
+          <GridItem xs={12} sm={12} md={4}>
+            <Card plain>
+              <GridItem xs={12} sm={12} md={6} className={classes.itemGrid}>
+                <img
+                  src={profilePhoto}
+                  alt="Paweł Kisielewicz"
+                  className={imageClasses}
+                />
+              </GridItem>
+              <h4 className={classes.cardTitle}>
+                mgr inż. Paweł Kisielewicz
+                <br />
+                <small className={classes.smallTitle}>
+                  Prowadzący koła naukowego COSMO
+                </small>
+              </h4>
+            </Card>
+          </GridItem>
+        </GridContainer>
+      </div>
+    </div>
+  );
 }
