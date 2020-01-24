73af4a08e4326c92e65fbea1d27191d402e11902
Jacob Chwastek <jacobchwastek@gmail.com>
2020-01-24 19:14:41 +0100

    Image, size , partners adjustment, rel="noopener noreferrer" in links (#20)

--- src/views/Components/Sections/OurTeamSection.js
+++ src/views/Components/Sections/OurTeamSection.js
@@ -1,6 +1,4 @@
 import React from "react";
-// nodejs library that concatenates classes
-import classNames from "classnames";
 // react components for routing our app without refresh
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
@@ -10,29 +8,25 @@ import Button from "components/CustomButtons/Button.js";
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
 import styles from "assets/jss/material-kit-react/views/landingPageSections/teamStyle.js";
-import Card from "components/Card/Card";
-import profilePhoto from "assets/img/faces/pawel_kisielewicz_profile.jpg";
+import photo from "assets/img/faces/zdjecieGrupowe.jpg"
 
 const useStyles = makeStyles(styles);
 
 export default function OurTeamSection(props) {
   const classes = useStyles();
-  const imageClasses = classNames(
-    classes.imgRaised,
-    classes.imgRoundedCircle,
-    classes.imgFluid
-  );
+
 
   //this style and style below is using for centering description paragraph vertically
   const descriptionGridContainerStyle = {
     height: "100%"
   };
 
-  // const descriptionGridItemStyle = {
-  //   height: "100%",
-  //   display: "flex",
-  //   alignItems: "center"
-  // };
+  const teamImgStyle = {
+    width:"100%",
+    height:"auto",
+    padding : "10px"
+  };
+
   const descriptionGridItemStyle2 = {
     height: "100%",
     display: "flex",
@@ -40,11 +34,6 @@ export default function OurTeamSection(props) {
     flexDirection: "column",
     justifyContent: "center"
   };
-  const mgrPictureStyle = {
-    maxWidth:"200px",
-    height:'auto',
-  };
-
   const descriptionStyle = {
     textAlign: "justify",
     fontFamily: "Roboto Slab",
@@ -70,7 +59,7 @@ export default function OurTeamSection(props) {
                 style={descriptionGridItemStyle2}
               >
                 <p className={classes.description} style={descriptionStyle}>
-                  Grupa młodych ludzi, studentów Politechniki Krakowskiej.
+                  Grupą młodych ludzi, studentów Politechniki Krakowskiej.
                   W przerwach od nauki spotykamy się by realizować wspólny cel - wysłać własnego satelitę na orbitę.
                   Nasze zmagania można obserwować na naszych kanałach w mediach społecznościowych. Trzymajcie kciuki!
                 </p>
@@ -82,23 +71,11 @@ export default function OurTeamSection(props) {
           </GridItem>
 
           <GridItem xs={12} sm={12} md={4}>
-            <Card plain>
-              <GridItem xs={12} sm={12} md={6} className={classes.itemGrid}>
                 <img
-                    style ={mgrPictureStyle}
-                  src={profilePhoto}
-                  alt="Paweł Kisielewicz"
-                  className={imageClasses}
+                  src={photo}
+                  alt="COSMO"
+                  style={teamImgStyle}
                 />
-              </GridItem>
-              <h4 className={classes.cardTitle}>
-                mgr inż. Paweł Kisielewicz
-                <br />
-                <small className={classes.smallTitle}>
-                  Prowadzący koła naukowego COSMO
-                </small>
-              </h4>
-            </Card>
           </GridItem>
         </GridContainer>
       </div>
