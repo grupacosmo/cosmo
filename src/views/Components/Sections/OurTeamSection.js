fdc615828760a1c15bb661f30950b06f3faad82c
borchowiec <borchowiecpatryk@gmail.com>
2020-07-06 19:38:50 +0200

    Self, instead of blank

--- src/views/Components/Sections/OurTeamSection.js
+++ src/views/Components/Sections/OurTeamSection.js
@@ -60,7 +60,7 @@ export default function OurTeamSection(props) {
                 style={descriptionGridItemStyle2}
               >
                 <p className={classes.description} style={descriptionStyle}>{locale.description}</p>
-                <Button color="primary" href="/team" target="_blank" round>{locale.moreButton}</Button>
+                <Button color="primary" href="/team" target="_self" round>{locale.moreButton}</Button>
               </GridItem>
             </GridContainer>
           </GridItem>
