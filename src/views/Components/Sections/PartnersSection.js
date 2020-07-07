1bdef9e24939259a226fee9bc571f0492cb0f35a
borchowiec <borchowiecpatryk@gmail.com>
2020-07-07 19:59:33 +0200

    Removed carousel with partners. Created list with partners.

--- src/views/Components/Sections/PartnersSection.js
+++ src/views/Components/Sections/PartnersSection.js
@@ -1,16 +1,16 @@
 import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import {Carousel} from 'react-responsive-carousel';
 import pk from "assets/img/partners/politechnika.jpg";
 import baltic from "assets/img/partners/balticsatapps.png";
-import department from "assets/img/partners/logoIT.png";
+import it from "assets/img/partners/it.PNG";
 import department2 from "assets/img/partners/katedra.png";
 import futureLab from "assets/img/partners/futurelab.png"
 import "assets/css/PartnerCarousel.css"
 import styles from "assets/jss/material-kit-react/views/landingPageSections/teamStyle.js";
 import {makeStyles} from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
 import getLocale from "../../../util/internationalization";
+import GridContainer from "../../../components/Grid/GridContainer";
+import GridItem from "../../../components/Grid/GridItem";
 
 const useStyles = makeStyles(styles);
 
@@ -20,7 +20,9 @@ const imgStyle = {
 
     padding: "auto",
     maxHeight: "150px",
-    maxWidth: "90%"
+    maxWidth: "90%",
+
+    display: "inline-block"
 };
 const imgStyle2 = {
     height: "auto",
@@ -31,81 +33,56 @@ const imgStyle2 = {
     maxWidth: "90%"
 };
 
-const gridStyle = {
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection:"column",
-
-};
-const itDepartmentStyle = {
-    color: "rgb(122,164,64)",
-
-};
-const carouselStyle = {
-    maxHeight: "150px",
-    width: "auto",
-    margin: "auto"
-};
-
-
-
 const PartnersSection = () => {
     const classes = useStyles();
     const locale = getLocale("index").partnersSection;
 
     return (
+      <div className={classes.section} id="our-partners">
+          <h2 className={classes.title}>{locale.title}</h2>
+          <GridContainer justify="center">
+              <GridItem xs={12}>
+                  <a href="https://botland.com.pl" target="_blank" rel="noopener noreferrer">
+                      <img src="https://botland.com.pl/img/logo_botland_kn.png"
+                           alt="Sklep dla robotyków i elektroników, Arduino, Raspberry Pi - BOTLAND"
+                           style={{width: "100%", maxWidth: "900px", padding: "10px"}}
+                      />
+                  </a>
+              </GridItem>
+          </GridContainer>
+          <GridContainer justify="center" style={{marginTop: "50px"}}>
+              <GridItem sm={12} style={{width: "100%", maxWidth: "700px"}}>
+                  <a href="https:\\www.pk.edu.pl" target="_blank" rel="noopener noreferrer">
+                      <img style={imgStyle} src={pk} alt="politechnika krakowska"/>
+                  </a>
+              </GridItem>
+          </GridContainer>
+          <GridContainer justify="center" style={{marginTop: "30px"}}>
+              <GridItem sm={12} style={{width: "100%", maxWidth: "700px"}}>
+                  <a href="https://ii.pk.edu.pl" target="_blank" rel="noopener noreferrer">
+                      <img style={imgStyle2} src={department2} alt="itdepartment"/>
+                  </a>
+              </GridItem>
+          </GridContainer>
+          <GridContainer justify="center" style={{marginTop: "30px"}}>
+              <GridItem xs={12} sm={6} md={4}>
+                  <a href="https://it.pk.edu.pl" target="_blank" rel="noopener noreferrer">
+                      <img style={imgStyle} src={it} alt="it"/>
+                  </a>
+              </GridItem>
+              <GridItem xs={12} sm={6} md={4}>
+                  <a href="https://balticsatapps.pl/" target="_blank" rel="noopener noreferrer">
+                      <img style={imgStyle} src={baltic} alt="balticsatapps"/>
+                  </a>
+              </GridItem>
+              <GridItem xs={12} sm={6} md={4}>
+                  <a href="http://futurelab.pk.edu.pl/" target="_blank" rel="noopener noreferrer">
+                      <img style={imgStyle} src={futureLab} alt="futurelab"/>
+                  </a>
+              </GridItem>
 
-        <div className={classes.section} id="our-partners">
-
-            <h2 className={classes.title}>{locale.title}</h2>
-
-
-            <Carousel style={carouselStyle}
-                      showArrows={false}
-                      interval={3000}
-                      infiniteLoop={true}
-                      showThumbs={false}
-                      autoPlay
-                      showIndicators={false}
-            >
-
-                <a href="https:\\www.pk.edu.pl" target="_blank" rel="noopener noreferrer">
-                    <div className="Partner">
-                        <img style={imgStyle} src={pk} alt="politechnika krakowska"/>
-                    </div>
-                </a>
-                <a href="https://balticsatapps.pl/" target="_blank" rel="noopener noreferrer">
-                    <div className="Partner">
-                        <img style={imgStyle} src={baltic} alt="balticsatapps"/>
-                    </div>
-                </a>
-                <a href="https://ii.pk.edu.pl" target="_blank" rel="noopener noreferrer">
-                    <div className="Partner">
-                        <img style={imgStyle2} src={department2} alt="itdepartment"/>
-                    </div>
-                </a>
-                <a href="https://it.pk.edu.pl" target="_blank" rel="noopener noreferrer">
-                    <div className="Partner">
-                        <Grid container  style={gridStyle}>
-                            <Grid xs={4} md={6}>
-                                <img style={imgStyle} src={department} alt="department"/>
-                            </Grid>
-                            <Grid xs={6} md={4}>
-                                <h2 style={itDepartmentStyle}>Wydział Informatyki i Telekomunikacji</h2>
-                            </Grid>
-                        </Grid>
-                    </div>
-                </a>
-                <a href="http://futurelab.pk.edu.pl/" target="_blank" rel="noopener noreferrer">
-                    <div className="Partner">
-                        <img style={imgStyle} src={futureLab} alt="futurelab"/>
-                    </div>
-                </a>
-
-            </Carousel>
-        </div>
-
-
+          </GridContainer>
+      </div>
     );
 };
 export default PartnersSection;
