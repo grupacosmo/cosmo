ef5a5a8af855891fc32ec7d684288c9c48e02a10
borchowiec <borchowiecpatryk@gmail.com>
2020-07-08 17:39:14 +0200

    Navbar changes color earlier

--- src/views/Components/Sections/BlogNavBarSection.js
+++ src/views/Components/Sections/BlogNavBarSection.js
@@ -11,7 +11,7 @@ export default function NavBarBlogSection(props) {
             fixed
             color="transparent"
             changeColorOnScroll={{
-                height: 500,
+                height: 400,
                 color: "white"
             }}
         />
